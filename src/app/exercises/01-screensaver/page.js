63dd9f969f1e8215f97732e3f313d960129a76a8
ser-emejia <emanuelxmejia@icloud.com>
2024-09-03 23:17:41 -0700

    ✅ Problem solved!

--- src/app/exercises/01-screensaver/page.js
+++ src/app/exercises/01-screensaver/page.js
@@ -1,11 +1,24 @@
-import React from 'react';
-
-import ScreenSaver from '../../../components/ScreenSaver';
+import React from "react";
+import Link from "next/link";
 
 function ScreenSaverExercise() {
   return (
-    <main className="screen-saver-wrapper">
-      <ScreenSaver color="red" />
+    <main>
+      <p>Choose your favorite color</p>
+      <ul>
+        <li>
+          <Link href={"/exercises/01-screensaver/red"}>Red</Link>
+        </li>
+        <li>
+          <Link href={"/exercises/01-screensaver/blue"}>Blue</Link>
+        </li>
+        <li>
+          <Link href={"/exercises/01-screensaver/green"}>Green</Link>
+        </li>
+        <li>
+          <Link href={"/exercises/01-screensaver/salmon"}>Salmon</Link>
+        </li>
+      </ul>
     </main>
   );
 }
--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,22 @@
-'use client';
-import React from 'react';
+"use client";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { ToastContext } from "../../../../components/ToastProvider/ToastProvider";
 
 function ContactPage() {
+  const router = useRouter();
+
+  const { createToast } = React.useContext(ToastContext);
+
   return (
     <main>
-      <form>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          router.back();
+          createToast("Your message was received", "success");
+        }}
+      >
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
